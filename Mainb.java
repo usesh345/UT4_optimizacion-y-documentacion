6e9278871757fd2fab312c4f8e5f7ae9fc2224f1
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-04-24 08:54:53 +0100

    codigo con metodos

--- Mainb.java
+++ Mainb.java
@@ -1,31 +1,44 @@
 public class Mainb {
-    public static void main(String[] args) {
-        int añoPrimo = 0;
-        int añoIncial = 2008;
-        while (añoIncial <= 2033) {
-            if (añoPrimo == 0) {
-                boolean esPrimo = true;
-                for (int i = añoIncial - 1; i > 1; i--) {
-                    if (añoIncial % i == 0) {
-                        esPrimo = false;
-                    }
-                }
-                if (esPrimo) {
-                    añoPrimo = añoIncial;
-                }
-            }
-            añoIncial++;
-            continue;
-        }
-        System.out.println("El primer año primo encontrado es " + añoPrimo);
-        if (añoPrimo % 2 != 0) {
-        } else System.out.println("que es un año par");
-        if (añoPrimo > 2008) {
-            if (añoPrimo < 2033) {
-                System.out.println("y está comprendido entre 2008 y 2033");
-            }
 
+    public static void main(String[] args) {
+
+        int inicio = 2008;
+        int fin = 2033;
+
+        int primerAnioPrimo = buscarPrimerAnioPrimo(inicio, fin);
+
+        mostrarResultados(primerAnioPrimo, inicio, fin);
+    }
+
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) return false;
+
+        for (int divisor = 2; divisor <= numero - 1; divisor++) {
+            if (numero % divisor == 0) {
+                return false;
+            }
         }
-        return;
+        return true;
+    }
+
+    public static int buscarPrimerAnioPrimo(int inicio, int fin) {
+        for (int anio = inicio; anio <= fin; anio++) {
+            if (esPrimo(anio)) {
+                return anio;
+            }
+        }
+        return 0;
+    }
+
+    public static void mostrarResultados(int anioPrimo, int inicio, int fin) {
+        System.out.println("El primer año primo encontrado es " + anioPrimo);
+
+        if (anioPrimo % 2 == 0) {
+            System.out.println("que es un año par");
+        }
+
+        if (anioPrimo > inicio && anioPrimo < fin) {
+            System.out.println("y está comprendido entre " + inicio + " y " + fin);
+        }
     }
 }

11fcd965fabe29da8b5a42ec20923f4bd1552e95
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-04-24 08:49:25 +0100

    clases renombradas

--- Mainb.java
+++ Mainb.java
@@ -1,29 +1,30 @@
 public class Mainb {
     public static void main(String[] args) {
-        int pp = 0;
-        int a = 2008;
-        while (a <= 2033) {
-            if (pp == 0) {
-                boolean ePr = true;
-                for (int i = a - 1; i > 1; i--) {
-                    if (a % i == 0) {
-                        ePr = false;
+        int añoPrimo = 0;
+        int añoIncial = 2008;
+        while (añoIncial <= 2033) {
+            if (añoPrimo == 0) {
+                boolean esPrimo = true;
+                for (int i = añoIncial - 1; i > 1; i--) {
+                    if (añoIncial % i == 0) {
+                        esPrimo = false;
                     }
                 }
-                if (ePr) {
-                    pp = a;
+                if (esPrimo) {
+                    añoPrimo = añoIncial;
                 }
             }
-            a++;
+            añoIncial++;
             continue;
         }
-        System.out.println("El primer año primo encontrado es " + pp);
-        if (pp % 2 != 0) {
+        System.out.println("El primer año primo encontrado es " + añoPrimo);
+        if (añoPrimo % 2 != 0) {
         } else System.out.println("que es un año par");
-        if (pp > 2008) {
-            if (pp < 2033) {
+        if (añoPrimo > 2008) {
+            if (añoPrimo < 2033) {
                 System.out.println("y está comprendido entre 2008 y 2033");
             }
+
         }
         return;
     }
